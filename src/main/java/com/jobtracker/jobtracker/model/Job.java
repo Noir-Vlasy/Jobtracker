e65c5a7afe3208d5f761e97bda1f72fa50e98f91
Shiv <Shivanshp705@gmail.com>
2026-04-09 19:33:27 +0530

    Job Tracker

---
+++ src/main/java/com/jobtracker/jobtracker/model/Job.java
@@ -0,0 +1,18 @@
+package com.jobtracker.jobtracker.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String company;
+    private String role;
+    private String status;
+    private String notes;
+}
